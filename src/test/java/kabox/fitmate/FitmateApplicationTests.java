47f91070a5281e06e6bf959f45cd6b235a595aa7
Kabox <kabox9@gmail.com>
2025-07-30 22:53:48 +0200

    first commit

---
+++ src/test/java/kabox/fitmate/FitmateApplicationTests.java
@@ -0,0 +1,13 @@
+package kabox.fitmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
